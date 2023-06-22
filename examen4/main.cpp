33627eb2e83ad06f8479c244e08210273a1a023b
Santiago <santiago.salas.sotillo@ucsp.edu.pe>
2023-06-22 00:29:06 -0500

    Resuelto

--- examen4/main.cpp
+++ examen4/main.cpp
@@ -6,10 +6,20 @@
 
 
 int main(){
+    Date d1{12, 27, 2010};
+    Date d2;
+
+
+    cout << "d1 es: " << d1 <<endl<<"d2 es " << d2;
+
+    d2.setDate(2, 28, 2008);
+
+    Date d3{7, 13, 2010};
+    cout << "++d3 es: " << ++d3 << endl;
 
 
 
 
 
-    
+
 }
